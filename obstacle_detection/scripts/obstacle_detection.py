3bcfee214c132e6a4a2237f6fa4f085ba0f061a7
Sreejith <sreejithmsreeni@gmail.com>
2022-04-14 17:13:58 +0900

    Add files via upload

--- obstacle_detection/scripts/obstacle_detection.py
+++ obstacle_detection/scripts/obstacle_detection.py
@@ -1,24 +1,30 @@
+#! /usr/bin/env python2
+from traceback import print_tb
 import rospy
 from sensor_msgs.msg import LaserScan
 
+class ObjectDetection:
 
-class ObjectDetection():
-    def _init_():
-        self.laser_sub = self.rospy.Subscriber('/scan',LaserScan,self.laser_callback)
+    def __init__(self):
+        rospy.init_node('laser_obstacle',anonymous=True)
+        self.laser_sub = rospy.Subscriber('/scan',LaserScan,self.laser_callback)
 
-    def laser_callback(data):
-        self.laser_data = data.ranges[i]
-        self.process_data(self.laser_data)
+    def laser_callback(self,data):
+        self.scan_data = data
+        self.process_data(self.scan_data)
     
-    def process_data(self,laser_ranges):
-        for i in ranges(30):
-            print('Laser data:',laser_ranges)
+    def process_data(self,scan_data):
+        self.i = -30        #To get the data from -30deg to 30 degree of laser
+        for self.i in range(30):
+            
+            self.range_data = scan_data.ranges[self.i]
+            # print('Laser data:',self.range_data)
+            self.i =self.i+1
 
-            # If the Distance to the obstacle is less than 0.5 meteres, its detected.
-            if laser_ranges < 0.5:
+            # If the Distance to the obstacle is less than 1.0 meteres, its detected.
+            if self.range_data < 1.0:
                 print("obstacle detectecd")    
 
 if __name__=='__main__':
-    rospy.init('Obstacle Detection Node',anonymous=True)
     ObjectDetection()
     rospy.spin()
